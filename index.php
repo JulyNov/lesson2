a8345e801b95bd75424b58044c719b835ff02049
July Nov <july@nov.so>
2017-03-01 01:34:04 +0200

    Задание 1

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
-// Задание 1
+//Задание #1
+//Функция должна принимать массив строк и выводить каждую строку в отдельном параграфе (тег <p>)
+//Если в функцию передан второй параметр true, то возвращать (через return) результат в виде одной объединенной строки.
+///
 
-echo 'Привет Второй урок';
+$StringArr = [
+    'Один',
+    'Два Два',
+    'Три Три ТРи',
+    'Четыре Четыре Четыре Четыре',
+    'Пять Пять Пять Пять Пять',
+];
+
+function Paragraf(array $stroki, bool $flag = false) {
+    if ($flag === true) {
+        return   implode(' ', $stroki);
+    }
+    foreach ($stroki as $value){
+        echo '<p>' . $value . '</p>';
+    }
+    return null;
+}
+
+echo Paragraf($StringArr);
+echo Paragraf($StringArr,true);
