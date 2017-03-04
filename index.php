97248756b47940fa685387cb61ad52802767053d
July Nov <july@nov.so>
2017-03-04 18:41:30 +0200

    Задание 2

--- index.php
+++ index.php
@@ -25,3 +25,67 @@ function Paragraf(array $stroki, bool $flag = false) {
 
 echo Paragraf($StringArr);
 echo Paragraf($StringArr,true);
+
+echo '<hr>';
+//Задание #2
+//
+//Функция должна принимать 2 параметра:
+//массив чисел, строку, обозначающую арифметическое действие,    которое нужно выполнить со всеми элементами массива.
+//Функция должна вывести результат на экран.
+//Функция должна обрабатывать любой ввод, в том числе некорректный и выдавать сообщения об этом
+
+
+$NumberArr = [1, 2, 1, 4, 5, 6];
+$operation = '/';
+
+function Calc(array $numbers, $operation)
+{
+
+    echo implode($operation, $numbers) . '=';
+    $summ = $numbers[0];
+
+    $i = 0;
+    foreach ($numbers as $value) {
+        $i++;
+        if ($i === 1) {
+            continue;
+        }
+        if (!ctype_digit((string)$value)) {
+            throw new ErrorException('Неккоректное число');
+        }
+        switch ($operation) {
+            case '+':
+                $summ += $value;
+                break;
+            case '-':
+                $summ -= $value;
+                break;
+            case '*':
+                $summ *= $value;
+                break;
+            case '/':
+                $summ /= $value;
+                break;
+            default:
+                throw new ErrorException('Неверная арифметическая операция');
+
+        }
+    }
+    echo $summ;
+}
+
+
+try {
+    Calc($NumberArr, $operation);
+} catch (Exception $exception) {
+    echo $exception->getMessage() . '<br>';
+}
+
+/*Задание #3
+
+Функция должна принимать переменное число аргументов.
+Первым аргументом обязательно должна быть строка, обозначающая арифметическое действие, которое необходимо выполнить со всеми передаваемыми аргументами.
+Остальные аргументы это целые и/или вещественные числа.
+
+Пример вызова: calcEverything(‘+’, 1, 2, 3, 5.2);
+Результат: 1 + 2 + 3 + 5.2 = 11.2*/
